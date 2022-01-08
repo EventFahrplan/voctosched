74476d053b987e72b64d7556955b386cfc0ba58a
Tobias Preuss <tobias.preuss@googlemail.com>
2022-01-08 15:47:37 +0100

    Customize schedule XML for FOSDEM 2021.

--- helper_scripts/fix_frab_date_tag.py
+++ helper_scripts/fix_frab_date_tag.py
@@ -3,7 +3,8 @@ import urllib.request
 import sys
 from time import strftime, localtime
 
-schedule_url = "https://fosdem.org/2020/schedule/xml"
+schedule_url = "https://fosdem.org/2021/schedule/xml"
+time_zone_name = "Europe/Brussels"
 print( "## getting meta data from " + schedule_url + " ##")
 global frab_data
 try:
@@ -21,6 +22,10 @@ formatted_download_time = strftime("%Y-%m-%d %H:%M", download_time)
 root.append(ET.Element('version'))
 root.find('version').text = formatted_download_time
 
+conference = root.find('conference')
+conference.append(ET.Element('time_zone_name'))
+conference.find('time_zone_name').text = time_zone_name
+
 for day in root.iter('day'):
     date = day.attrib['date']
     day.set('end', date + "T05:00:00+01:00")
@@ -31,10 +36,10 @@ for day in root.iter('day'):
         event.find('date').text = date +  "T" + event.find('start').text + ":00+01:00"
 
 # Local test with time stamp suffix
-tree.write("schedule-" + strftime("%Y%m%d_%H%M", download_time) + ".xml")
+# tree.write("schedule-" + strftime("%Y%m%d_%H%M", download_time) + ".xml")
 
 # Production path
-# tree.write("/home/opentree/html/fosdem2020/schedule.xml")
+tree.write("/home/metadude/html/fosdem2021/schedule.xml")
 
 # Log output
 print( "## downloaded file from " + schedule_url + " ## " + strftime("%Y%m%d_%H%M", download_time))
